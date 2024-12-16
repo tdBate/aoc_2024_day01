dfac6d5edf1c845d00f80e102a8581a727b8ae99
Torok Donat <td.zazsa@gmail.com>
2024-12-16 18:49:33 +0100

    file read in

--- Program.cs
+++ Program.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
 
 namespace aoc_2024_day01
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("a");
+            List<int> bal = new List<int>();
+            List<int> jobb = new List<int>();
+
+            string[] lines = File.ReadAllLines("../../input.txt");
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Replace("\n", "").Replace("   ",";").Split(';');
+                bal.Add(int.Parse(parts[0]));
+                jobb.Add(int.Parse(parts[1]));
+            }
+
             Console.ReadKey();
         }
     }
