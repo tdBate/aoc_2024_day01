07a97ac6368349f8284bdbc877051e4f1dcbfcc2
Torok Donat <td.zazsa@gmail.com>
2024-12-17 14:41:24 +0100

    Old method redo witg fake input

--- Program.cs
+++ Program.cs
@@ -45,21 +45,56 @@ namespace aoc_2024_day01
         {
             //part1();
 
-            List<int> bal = new List<int>();
-            List<int> jobb = new List<int>();
+            List<string> bal = new List<string>();
+            List<string> jobb = new List<string>();
 
-            string[] lines = File.ReadAllLines("../../input.txt");
+            string[] lines = File.ReadAllLines("../../input_fake.txt");
 
             foreach (string line in lines)
             {
                 string[] parts = line.Replace("\n", "").Replace("   ", ";").Split(';');
-                bal.Add(int.Parse(parts[0]));
-                jobb.Add(int.Parse(parts[1]));
+                bal.Add(parts[0]);
+                jobb.Add(parts[1]);
             }
-            bal.Sort();
-            jobb.Sort();
+
+
+            int osszeg = 0;
+
+            //betűkre osztás
+            int[] balelemek = new int[6];
+            int[] jobelemek = new int[6];
+            for (int a = 0; a < bal.Count; a++)
+            {
+                for (int b = 0; b < bal[a].Length; b++)
+                {
+                    Console.WriteLine(int.Parse(bal[a][b] + ""));
+                    balelemek[b] = int.Parse(bal[a][b] + "");
+                    jobelemek[b] = int.Parse(jobb[a][b] + "");
+                }
+                Array.Sort(balelemek);
+                Array.Sort(jobelemek);
+
+                for (int b = 0; b < bal[a].Length; b++)
+                {
+
+                    osszeg += Math.Abs(balelemek[b] - jobelemek[b]);
+                    Console.WriteLine(balelemek[b] + " " + jobelemek[b]);
+                    //Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine(osszeg);
+
+            foreach (int elem in balelemek)
+            {
+                //Console.WriteLine(elem);
+            }
+
+
+
 
             Console.ReadKey();
         }
+
     }
 }
